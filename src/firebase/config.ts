ecc19bff206c9a0153a92ef167ffc6e0271d84cc
Rabbuni Merugumala <rabbuni144@gmail.com>
2025-10-31 17:52:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5989045864-1cb4d",
+  "appId": "1:566644776035:web:082cdb67468e9d0411ec56",
+  "apiKey": "AIzaSyANn1g8FW2WCr_XyWbU89YmFtUVCp3wjsA",
+  "authDomain": "studio-5989045864-1cb4d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "566644776035"
+};
